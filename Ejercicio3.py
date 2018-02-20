a2abb09fdf555b5189504ccd0c70a0605bc8af1d
SCVA <scva97@hotmail.com>
2018-02-19 22:10:43 -0500

    Add files via upload

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -5,10 +5,13 @@ def insertar(arbol,valor):
     if arbol == None:
         return Nodo(valor)
     if comparar(arbol.valor):
-        return Nodo(arbol.valor,insertar(arbol.izq, valor),arbol.der)
+        return Nodo(arbol.valor,arbol.izq,insertar(arbol.der, valor))
     else:
-        return Nodo(arbol.valor,arbol.izq,insertar(arbol.der, valor)) 
-        
+        if(arbol.der == None):
+            return Nodo(arbol.valor,arbol.izq,insertar(arbol.der, valor))
+        else:
+            return Nodo(arbol.valor,insertar(arbol.izq, valor),arbol.der)
+                    
 def comparar(elemento):
     if elemento=='+' or elemento=='-' or elemento=='*' or elemento=='/':
         return True
@@ -33,14 +36,15 @@ def crearPila(cadena):
     [lista.apilar(x) for x in elementos]
     return lista
 
-def crearArbol(arbolito,pilita):
-    if pilita.es_vacia():
-        return arbolito
-    else:
-        arbolito = insertar(arbolito, pilita.desapilar())
-        return crearArbol(arbolito,pilita)
+
+def crearArbol(pilita):
+    arbol = Nodo(pilita.desapilar())
+    while not(pilita.es_vacia()):
+        arbol = insertar(arbol,pilita.desapilar())
+        print preorden(arbol)
+    return arbol
 
 def iniciar(entrada):
-    print evaluar(crearArbol(None,crearPila(entrada)))
+        print evaluar(crearArbol(crearPila(entrada)))
 
 print(iniciar("8 2 - 9 7 + *"))
